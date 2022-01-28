fd3ad74752233021d63784f7bde34e2f6b5f5d99
gyeneics12d <gyeneics.12d@acsjszki.hu>
2022-01-28 09:33:52 +0100

    2. feladat

--- 2022-01-28_Opening-weekend/OpeningWeekend.cs
+++ 2022-01-28_Opening-weekend/OpeningWeekend.cs
@@ -7,15 +7,40 @@ using System.IO;
 
 namespace _2022_01_28_Opening_weekend
 {
-    class Előadások
+    class Film
     {
+        public string eredetiCim { get; set; }
+        public string magyarCim { get; set; }
+        public string bemutato { get; set; }
+        public string forgalmazo { get; set; }
+        public int bevetel { get; set; }
+        public int latogatok { get; set; }
 
+        public Film(string sor)
+        {
+            string[] t = sor.Split(';');
+            eredetiCim = t[0];
+            magyarCim = t[1];
+            bemutato = t[2];
+            forgalmazo = t[3];
+            bevetel = int.Parse(t[4]);
+            latogatok = int.Parse(t[4]);
+
+        }
     }
 
     class OpeningWeekend
     {
         static void Main(string[] args)
         {
+            //2
+            List<Film> filmek = new List<Film>();
+            foreach (var sor in File.ReadAllLines("nyitohetvege.txt").Skip(1))
+            {
+                filmek.Add(new Film(sor));
+            }
+
+            Console.ReadKey();
         }
     }
 }
