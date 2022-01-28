02e021adbb765047648094f90ad9f9d628a2fcf8
gyeneics12d <gyeneics.12d@acsjszki.hu>
2022-01-28 09:10:38 +0100

    Add project files.

---
+++ 2022-01-28_Opening-weekend/OpeningWeekend.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2022_01_28_Opening_weekend
+{
+    class Előadások
+    {
+
+    }
+
+    class OpeningWeekend
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
